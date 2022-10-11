323b2f472389fc9026bf7b19c2a6bd9031fcfad3
Kęstutis Jakaitis <kestutis.jakaitis@gmail.com>
2022-10-11 20:52:29 +0300

    Task 3 Finished

--- Task 3/script.js
+++ Task 3/script.js
@@ -12,3 +12,52 @@ turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'https://api.github.com/users';
+
+
+
+function createTable(){
+    const thead = document.createElement('thead');
+    const login = document.createElement('th');
+  thead.append(tr);
+    const picture = document.createElement('th');
+    const tr = document.createElement('tr');
+  tr.append(login, picture);
+    const table = document.createElement('table');
+  table.append(thead, document.createElement('tbody'));
+  let divOutput = document.getElementById("output");
+  divOutput.appendChild(table);
+}
+function data(people){
+	try{
+	const body = document.getElementById("output");
+	people.forEach(people => {
+		const div = document.createElement('div');
+		const login = document.createElement('div');
+		login.innerText = people.login;
+		const picture = document.createElement('img');
+		picture.src = people.avatar_url;
+		const picturediv = document.createElement('div');
+		picturediv.append(picture);
+        
+		div.append(login);
+		div.append(picturediv);
+		body.append(div);
+	});
+	}catch(error){
+		console.error(error);
+	}
+}
+fetch(ENDPOINT)
+    .then(result => result.json())
+    .then((output) => {
+		list = output;      
+}).catch(error => console.error(error));
+const element = document.getElementById("btn");
+element.addEventListener("click",show);
+
+let list = {};
+
+function show(){
+	data(list);
+    document.getElementById("message").style.visibility="hidden";
+}
