77b55330c44c26b579c8114d869f21a2cd770c31
Kęstutis Jakaitis <kestutis.jakaitis@gmail.com>
2022-10-11 19:42:51 +0300

    task 1 finished

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,23 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+function lbCalculator(kg){
+    return kg * 2.2046
+};
+function gCalculator(kg){
+    return kg / 0.0010000
+};
+function ozCalculator(kg){
+    return kg * 35.274
+};
+const weightInfo = document.querySelector("form");
+weightInfo.addEventListener("submit", info);
+
+function info(e){
+    e.preventDefault();
+    const weightInfo = document.getElementById("search").value;
+    document.querySelector("li:nth-of-type(1)").innerHTML = "Svarai(lb) " + lbCalculator(weightInfo);
+    document.querySelector("li:nth-of-type(2)").innerHTML = "Gramai(g) " + gCalculator(weightInfo);
+    document.querySelector("li:nth-of-type(3)").innerHTML = "Uncijos(oz) " + ozCalculator(weightInfo);
+}
