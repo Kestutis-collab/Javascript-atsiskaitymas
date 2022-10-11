fe39e11a3f5a9d96ccaaab6ad7731443a7d17828
Kęstutis Jakaitis <kestutis.jakaitis@gmail.com>
2022-10-11 21:19:20 +0300

    Task 4 Finished

--- Task 4/script.js
+++ Task 4/script.js
@@ -10,3 +10,28 @@ turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+
+function info(a){
+	const body = document.getElementById("output");
+	for(let i = 0; i < a.length; i++){
+		const div = document.createElement('div');
+		const bla = document.createElement('div');
+        div.append(bla);
+        bla.classList.add("bla");
+		bla.innerText = a[i].brand;
+		const brand = document.createElement('div');
+        div.append(brand);
+        brand.classList.add("brand");
+		brand.innerText = a[i].models;
+		body.append(div);
+	}	
+}
+let cars = {};
+fetch(ENDPOINT)
+    .then(result => result.json())
+    .then((output) => {
+		cars = output;
+		info(cars);
+        
+});
